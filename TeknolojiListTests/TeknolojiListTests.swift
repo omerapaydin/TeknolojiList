c38b56319a848041f7059264db8cdc60ac24335a
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-11 00:22:40 +0300

    Initial Commit

---
+++ TeknolojiListTests/TeknolojiListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeknolojiListTests.swift
+//  TeknolojiListTests
+//
+//  Created by Ömer on 11.04.2025.
+//
+
+import Testing
+@testable import TeknolojiList
+
+struct TeknolojiListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
