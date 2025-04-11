df5f8b4e842a967d2050153389efe937e5108e32
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-11 20:21:49 +0300

    Cell

---
+++ TeknolojiList/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  TeknolojiList
+//
+//  Created by Ömer on 11.04.2025.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TeknolojiList/Tech.swift
@@ -0,0 +1,8 @@
+//
+//  Tech.swift
+//  TeknolojiList
+//
+//  Created by Ömer on 11.04.2025.
+//
+
+import Foundation
